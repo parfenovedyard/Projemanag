95d7a63a3852019795185f87e8e3fb8e69f77ea9
parfenovedyard <parfenovedyard@gmail.com>
2021-07-06 15:19:47 +0300

    setup intro screen

--- app/src/main/java/com/example/progemanage/SplashActivity.kt
+++ app/src/main/java/com/example/progemanage/SplashActivity.kt
@@ -1,8 +1,10 @@
 package com.example.progemanage
 
+import android.content.Intent
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.text.style.TypefaceSpan
 import android.view.WindowManager
 import android.widget.TextView
@@ -18,5 +20,11 @@ class SplashActivity : AppCompatActivity() {
         )
         val typeFace: Typeface = Typeface.createFromAsset(assets,"carbon bl.ttf")
         findViewById<TextView>(R.id.tv_app_name).typeface = typeFace
+
+        Handler().postDelayed({
+            startActivity(Intent(this, IntroActivity::class.java))
+            finish()
+        }, 2500)
     }
+
 }
