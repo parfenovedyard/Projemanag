3b40ce9411314d0d5c8f2f55cac5d07f2f803832
parfenovedyard <parfenovedyard@gmail.com>
2021-07-07 16:37:29 +0300

    setup sing up activity

--- app/build.gradle
+++ app/build.gradle
@@ -37,6 +37,9 @@ android {
             }
         }
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
--- app/src/main/java/com/example/projemanag/IntroActivity.kt
+++ app/src/main/java/com/example/projemanag/IntroActivity.kt
@@ -1,18 +1,27 @@
 package com.example.projemanag
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import com.example.projemanag.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityIntroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
+        binding.btnSignUpIntro.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
     }
 }
