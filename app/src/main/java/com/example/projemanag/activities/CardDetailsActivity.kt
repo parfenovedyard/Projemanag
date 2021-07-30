e55d4a8e3e6e91cc11cc886c616123f511e1eeba
parfenovedyard <parfenovedyard@gmail.com>
2021-07-30 15:06:53 +0300

    Loading Card Details To Set The Title Of The Card

--- app/src/main/java/com/example/projemanag/activities/CardDetailsActivity.kt
+++ app/src/main/java/com/example/projemanag/activities/CardDetailsActivity.kt
@@ -2,11 +2,52 @@ package com.example.projemanag.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.example.projemanag.R
+import com.example.projemanag.databinding.ActivityCardDetailsBinding
+import com.example.projemanag.models.Board
+import com.example.projemanag.utils.Constants
 
 class CardDetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCardDetailsBinding
+    private lateinit var mBoardDetails: Board
+    private var mTaskListPosition = -1
+    private var mCardPosition = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_card_details)
+        binding = ActivityCardDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        getIntentData()
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(binding.toolbarCardDetailsActivity)
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+            actionBar.title = mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].name
+        }
+        binding.toolbarCardDetailsActivity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    private fun getIntentData() {
+        if (intent.hasExtra(Constants.BOARD_DETAIL)) {
+            mBoardDetails = intent.getParcelableExtra(Constants.BOARD_DETAIL)!!
+        }
+        if (intent.hasExtra(Constants.TASK_LIST_ITEM_POSITION)) {
+            mTaskListPosition = intent.getIntExtra(Constants.TASK_LIST_ITEM_POSITION, -1)
+        }
+        if (intent.hasExtra(Constants.CARD_LIST_ITEM_POSITION)) {
+            mCardPosition = intent.getIntExtra(Constants.CARD_LIST_ITEM_POSITION, -1)
+        }
     }
 }
